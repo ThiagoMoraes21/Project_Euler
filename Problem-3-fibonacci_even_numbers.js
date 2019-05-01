be51ce41cb2fbe2c94cf674987a403e8ab947b26
Thiago Moraes <thiago4x70@gmail.com>
2019-05-01 19:49:26 -0300

    Problem 3 fibonacci even numbers

---
+++ Problem-3-fibonacci_even_numbers.js
@@ -0,0 +1,21 @@
+function fibo(n){
+    let fib = [0, 1];
+	let total = 0;
+	for(let i = 0; i <= n; i++){
+		if(fib[i] == 0){
+            fib[i + 2] = fib[i] + fib[i + 1];
+		} else {
+            fib[i + 2] = fib[i + 1] + fib[i];
+            if(fib[i + 2] % 2 == 0) {
+                console.log('Even number: ' + fib[i + 2]); 
+                total += fib[i + 2];
+            }
+        }
+    }
+    console.log('Total: ' + total);
+    console.log('Fibonacci sequence: ' + fib);
+    return total;
+}
+
+fibo(10);
+
